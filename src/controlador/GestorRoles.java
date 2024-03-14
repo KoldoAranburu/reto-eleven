6ad2ece3d5494f9e1d321aeb6323c6efc9669cd7
KoldoAranburu <149675361+KoldoAranburu@users.noreply.github.com>
2024-03-14 11:44:07 +0100

    nuevos gestores

--- src/controlador/GestorRoles.java
+++ src/controlador/GestorRoles.java
@@ -1,5 +1,9 @@
 package controlador;
 
+import java.util.Scanner;
+
+import vista.Menu;
+
 public class GestorRoles {
 
 	public static void main(String[] args) {
@@ -9,6 +13,31 @@ public class GestorRoles {
 	}
 
 	public static void run() {
+		Scanner scan = new Scanner(System.in);
+		int opcion;
 		
+		do {
+			Menu.menuPrincipal();
+			opcion = Integer.parseInt(scan.nextLine());
+			
+			switch (opcion) {
+			case Menu.SALIR:
+				System.out.println("Saliendo...");
+				break;
+			case Menu.USUARIO:
+				
+				break;
+			case Menu.ADMIN:
+				
+				break;
+
+			default:
+				System.out.println("Opción incorrecta, intentelo de nuevo.");
+				break;
+			}
+			
+			
+			
+		} while (opcion != Menu.SALIR);
 	}
 }
