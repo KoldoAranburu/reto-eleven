8704930454add14b5039adbc08cf52aeb11d566a
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-03-14 11:39:13 +0100

    falta testear el conector

--- src/modelo/Conector.java
+++ src/modelo/Conector.java
@@ -1,5 +1,35 @@
 package modelo;
 
-public class Conector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conector {
+	private final static String BBDD = "reto_caballeros";
+	private final static String USUARIO="root";
+	private final static String CONTRASEÑA ="";
+	private final static String HOST = "localhost";
+	private static Connection CONEXIONBBDD = null;
+	
+	private Conector() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			CONEXIONBBDD = DriverManager.getConnection("jdbc:mysql://" + HOST + "/" + BBDD,USUARIO,CONTRASEÑA);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+	}
+	
+	private Connection getConexion() {
+		return CONEXIONBBDD;
+	}
+	private void deconectar() {
+		try {
+			CONEXIONBBDD.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
